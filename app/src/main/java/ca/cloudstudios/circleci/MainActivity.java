7c05cbca8d3aa85c026a17dce22abfb06f0a1023
Bhaskar Murthy <bmurthy@codified.co>
2015-04-12 02:28:57 -0400

    add dummy content to navigation drawer

--- app/src/main/java/ca/cloudstudios/circleci/MainActivity.java
+++ app/src/main/java/ca/cloudstudios/circleci/MainActivity.java
@@ -1,32 +1,33 @@
 package ca.cloudstudios.circleci;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 
-import com.heinrichreimersoftware.materialdrawer.DrawerFrameLayout;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.accountswitcher.AccountHeader;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.util.KeyboardUtil;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 
-public class MainActivity extends ActionBarActivity {
-
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
-    @InjectView(R.id.drawer)
-    DrawerFrameLayout mDrawer;
+public class MainActivity extends ActionBarActivity implements Drawer.OnDrawerItemClickListener, Drawer.OnDrawerListener {
 
     @InjectView(R.id.toolbar)
     Toolbar mToolbar;
 
-    private ActionBarDrawerToggle mDrawerToggle;
+    private Drawer.Result mDrawer;
+
+    private AccountHeader.Result mDrawerHeader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,28 +35,55 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
 
-        mDrawerToggle = new ActionBarDrawerToggle(
-                this,
-                mDrawer,
-                mToolbar,
-                R.string.navigation_drawer_open,
-                R.string.navigation_drawer_close
-        ) {
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                invalidateOptionsMenu();
-            }
-
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                invalidateOptionsMenu();
-            }
-        };
-
         setSupportActionBar(mToolbar);
 
-        mDrawer.setDrawerListener(mDrawerToggle);
-        mDrawer.closeDrawer();
+        mDrawerHeader = new AccountHeader()
+                .withActivity(this)
+                .withHeaderBackground(getResources().getDrawable(R.drawable.cat_background, null))
+                .addProfiles(
+                        new ProfileDrawerItem()
+                                .withName("circleci-android")
+                                .withEmail("bhaskarmurthy")
+                                .withIcon(getResources().getDrawable(R.drawable.cat, null)),
+                        new ProfileDrawerItem()
+                                .withName("bhaskarmurthy.github.io")
+                                .withEmail("bhaskarmurthy")
+                                .withIcon(getResources().getDrawable(R.drawable.cat, null))
+                )
+                .withProfileImagesClickable(false)
+                .withSavedInstance(savedInstanceState)
+                .build();
+
+        mDrawer = new Drawer()
+                .withActivity(this)
+                .withToolbar(mToolbar)
+                .withAccountHeader(mDrawerHeader)
+                .addDrawerItems(
+                        new PrimaryDrawerItem()
+                            .withName("My branches"),
+                        new PrimaryDrawerItem()
+                            .withName("All branches"),
+                        new DividerDrawerItem(),
+                        new PrimaryDrawerItem()
+                                .withName("develop")
+                                .withDescription("Build #1157: Success")
+                                .withBadge("1"),
+                        new PrimaryDrawerItem()
+                                .withName("master")
+                                .withDescription("Build #1162: Timed out"),
+                        new DividerDrawerItem(),
+                        new SecondaryDrawerItem()
+                                .withName("Settings"),
+                        new SecondaryDrawerItem()
+                                .withName("Help & feedback")
+        )
+        .withFireOnInitialOnClick(true)
+                .withSavedInstance(savedInstanceState)
+                .withOnDrawerListener(this)
+                .withOnDrawerItemClickListener(this)
+                .build();
+
+        mDrawer.keyboardSupportEnabled(this, true);
     }
 
     @Override
@@ -65,23 +93,34 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
+    protected void onSaveInstanceState(Bundle outState) {
+        outState = mDrawerHeader.saveInstanceState(outState);
+        outState = mDrawer.saveInstanceState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawer != null && mDrawer.isDrawerOpen()) {
+            mDrawer.closeDrawer();
+            return;
         }
 
-        return super.onOptionsItemSelected(item);
+        super.onBackPressed();
     }
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l, IDrawerItem iDrawerItem) {
+
     }
 
     @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        mDrawerToggle.syncState();
+    public void onDrawerOpened(View view) {
+        KeyboardUtil.hideKeyboard(this);
+    }
+
+    @Override
+    public void onDrawerClosed(View view) {
+
     }
 }
